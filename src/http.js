9c9101e9d8cc10930a4019686e789cfe297c4d29
刘建辉 <jianhui@yzyw-inc.com>
2019-12-15 16:42:57 +0800

    init

---
+++ src/http.js
@@ -0,0 +1,43 @@
+import axios from "axios"
+import Vue from 'vue'
+import router from './router'
+
+const http = axios.create({
+  baseURL: process.env.VUE_APP_API_URL || '/admin/api'
+  // baseURL: 'http://localhost:3000/admin/api'
+})
+
+// Add a request interceptor
+http.interceptors.request.use((config) => {
+  // Do something before request is sent
+  const token = localStorage.getItem('token') || ''
+  if (token) {
+    config.headers.Authorization = 'Bearer ' + token
+  }
+  
+  return config;
+}, (error) => {
+  // Do something with request error
+  return Promise.reject(error);
+});
+
+// Add a response interceptor
+http.interceptors.response.use((response) => {
+  // Do something with response data
+  return response;
+}, (error) => {
+  if (error.response.status === 422) {
+    Vue.prototype.$message({
+      type: 'error',
+      message: error.response.data.message
+    })
+  }
+  if (error.response.status === 401) {
+    router.push('/login')
+  }
+
+  // Do something with response error
+  return Promise.reject(error);
+});
+
+export default http;
